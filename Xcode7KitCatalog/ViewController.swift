97304de523fb65fd337b6324c8bfe031f38b1c9d
nobukazu <nobukazu@nobukazu-no-iMac.local>
2016-01-30 19:40:54 +0900

    初回コミット

---
+++ Xcode7KitCatalog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode7KitCatalog
+//
+//  Created by nobukazu on 2016/01/30.
+//  Copyright © 2016年 nobukazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
