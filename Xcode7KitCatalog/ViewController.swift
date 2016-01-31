ba2e4eeba286ccbfa12380c7ae7a8419b451947e
nobukazu <nobukazu@nobukazu-no-iMac.local>
2016-01-31 15:29:59 +0900

    テーブル表示処理追加

--- Xcode7KitCatalog/ViewController.swift
+++ Xcode7KitCatalog/ViewController.swift
@@ -8,16 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let texts = ["東京", "横浜", "小田原", "静岡", "名古屋"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        tableView.tableFooterView = UIView(frame: CGRectMake(0, 0, 0, 0))
+        
+//        print(texts.description)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return texts.count
+    }
+    
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = texts[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        let text: String = texts[indexPath.row]
+        print(text)
+    }
+
 }
 
